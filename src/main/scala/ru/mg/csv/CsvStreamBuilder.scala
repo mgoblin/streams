4e829a72f1074c7710abca5aae5f39d819fe6913
mike <mike.golovanov@gmail.com>
2018-01-01 14:35:05 +0300

    add debug logging to CsvStreamBuilder

--- src/main/scala/ru/mg/csv/CsvStreamBuilder.scala
+++ src/main/scala/ru/mg/csv/CsvStreamBuilder.scala
@@ -8,14 +8,23 @@ import org.apache.flink.core.fs.Path
 import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment}
 import org.apache.flink.types.Row
 
+/**
+  * Build DataStream from CSV file
+  *
+  * @param filePath CSV file path and name
+  */
 class CsvStreamBuilder(filePath: String) extends LazyLogging {
 
   def build(env: StreamExecutionEnvironment, fields: Array[TypeInformation[_]]): DataStream[Row] = {
 
+    logger.debug(s"Build csv stream for $filePath")
+
     val path = new Path(filePath)
-
     val input = new RowCsvInputFormat(path, fields)
+    val rowStream = env.readFile(input, filePath)
 
-    env.readFile(input, filePath)
+    logger.debug(s"Csv stream for $filePath build done")
+
+    rowStream
   }
 }
