b08ef96fd7da35253300558a7c5eae8593121c0d
mike <mike.golovanov@gmail.com>
2018-01-04 09:20:12 +0300

    frequent outgoings unit tests

--- src/test/scala/ru/mg/detectors/FrequentOutgoingSpec.scala
+++ src/test/scala/ru/mg/detectors/FrequentOutgoingSpec.scala
@@ -15,7 +15,7 @@ import ru.mg.utils.{FileUtils, SinkCollector}
 
 @RunWith(classOf[JUnitRunner])
 class FrequentOutgoingSpec extends FlatSpec with Serializable with Matchers {
-  "frequent outgoing detector" should "search payments from person in sliding window" in {
+  "frequent outgoing detector" should "search frequent payments from person in sliding window" in {
     val tempFile = FileUtils.createTempFile("payments.csv")
 
     val env = StreamExecutionEnvironment.createLocalEnvironment(parallelism = 1)
@@ -43,6 +43,63 @@ class FrequentOutgoingSpec extends FlatSpec with Serializable with Matchers {
       Fraud(Person("Mike"), Seq(Payment(Person("Mike"), Person("Elly"), 100, date1)), "dumb"),
       Fraud(Person("Jack"), Seq(Payment(Person("Jack"), Person("Mike"), 150, date2)), "dumb"),
       Fraud(Person("Jack"), Seq(Payment(Person("Jack"), Person("Mike"), 150, date2)), "dumb")
+    )
+
+  }
+
+  it should "not search fraud if payments count less than threshold" in {
+    val tempFile = FileUtils.createTempFile("payments.csv")
+
+    val env = StreamExecutionEnvironment.createLocalEnvironment(parallelism = 1)
+    env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
+
+    val input = PaymentsStream(env, tempFile.getAbsolutePath)
+
+    SinkCollector.clear()
+    val collector = SinkCollector[Fraud]
+
+    val fraud = frequentOutgoings(1000, 500, 1)(input)
+
+    fraud.addSink(f => collector.add(f))
+
+    env.execute()
+
+    SinkCollector.collector should have size 0
+
+  }
+
+  it should "search fraud more than threshold in a sliding window" in {
+    val tempFile = FileUtils.createTempFile("freqPayments.csv")
+
+    val env = StreamExecutionEnvironment.createLocalEnvironment(parallelism = 1)
+    env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
+
+    val input = PaymentsStream(env, tempFile.getAbsolutePath)
+
+    SinkCollector.clear()
+    val collector = SinkCollector[Fraud]
+
+    val fraud = frequentOutgoings(10000, 8000, 1)(input)
+
+    fraud.addSink(f => collector.add(f))
+
+    env.execute()
+
+    SinkCollector.collector should have size 1
+
+    val sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS")
+    val date1 = sdf.parse("2018-01-01 12:19:00.000")
+    val date2 = sdf.parse("2018-01-01 12:19:03.000")
+
+    SinkCollector.collector should contain theSameElementsAs Seq(
+      Fraud(
+        Person("Mike"),
+        Seq(
+          Payment(Person("Mike"), Person("Elly"), 100, date1),
+          Payment(Person("Mike"), Person("Alice"), 120, date2)
+        ),
+        "dumb"
+      )
     )
 
   }
